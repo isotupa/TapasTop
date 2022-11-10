412c134f9576b854d5fa5e250c31b3e249aae7dd
diego <ellocodelaporteria178@gmail.com>
2022-11-10 16:03:02 +0100

    Añadido clases modelo y controlador

---
+++ app/src/main/java/com/example/tapastop/backend/sqlte/Controlador.java
@@ -0,0 +1,9 @@
+package com.example.tapastop.backend.sqlte;
+//Esta clase recibe los datos de entrada del usuario
+//a traves de la interfaz y llama al modelo para ejecutar la llamada
+
+public class Controlador {
+    public boolean crearCuenta(Usuario usuario){
+        return true;
+    }
+}
---
+++ app/src/main/java/com/example/tapastop/backend/sqlte/Modelo.java
@@ -0,0 +1,6 @@
+package com.example.tapastop.backend.sqlte;
+
+//En esta clase estara toda la logica de la app
+//
+public class Modelo {
+}
